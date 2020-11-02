39f6caa9daa74afc7e3fa4d9cb6749d39e715a58
yueyuescw09wing <73089418+yueyuescw09wing@users.noreply.github.com>
2020-11-02 11:06:44 +0800

    一些東西

--- main.ts
+++ main.ts
@@ -1,17 +1,22 @@
-let chance = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . # . #
-        # . . . #
-        # # # # #
-        `)
-    basic.showIcon(IconNames.StickFigure)
-    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
     chance = 3
     basic.showNumber(chance)
     while (chance > 0) {
-    	
+        if (input.pinIsPressed(TouchPin.P1)) {
+            music.playTone(523, music.beat(BeatFraction.Whole))
+            chance += -1
+            basic.showNumber(chance)
+        }
     }
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
 })
+let chance = 0
+basic.showString("Hi，nice to meet you")
